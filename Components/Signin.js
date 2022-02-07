ef5c273e0dd2e28ebe230992992dc910eb0f72e2
Luis Martinez <luis_mmartinez@live.com>
2022-02-06 19:33:28 -0500

    updated signin and signup code to allow production build (production build yielded errors due to some HTML rules violations in signin/signup forms)

--- Components/Signin.js
+++ Components/Signin.js
@@ -1,6 +1,8 @@
 import { Button, TextField } from '@mui/material';
 import { useState } from 'react'
 import React from 'react';
+import Link from 'next/link'
+
 
 const firstState = {
      "employeeId" : "",
@@ -19,15 +21,19 @@ function Signin() {
           //send form to backend for account creation
           setSignInForm(firstState)
      }
-  return <div id="signup-form">
+  return <form id="signup-form">
                <div >
                     <h1 id="signup-header">Sign In</h1>
                </div>
               <TextField required className="text-field" id="outlined-basic" name="employeeId" value={signInForm.first} onChange={(e)=> inputChange(e)} label="Employee ID" variant="outlined" />
-              <TextField required className="text-field" id="outlined-basic" type="password" name="password"value={signInForm.Password} onChange={(e) => inputChange(e)} label="Password" variant="outlined" />
+              <TextField required className="text-field" id="outlined-basic2" type="password" name="password"value={signInForm.Password} onChange={(e) => inputChange(e)} label="Password" variant="outlined" />
               <Button id="signup-button" color="success" onClick={submit}variant="contained">Sign In</Button>
-              <p>Don't have an account? <a href="/signup">Sign up. </a></p>
-          </div>;
+              <p>Don{"'"}t have an account?  
+                   <Link href="/signup">
+                         <a>Sign up. </a>
+                    </Link>
+               </p>
+          </form>;
 }
 
 export default Signin;
--- Components/Signup.js
+++ Components/Signup.js
@@ -1,6 +1,8 @@
 import { Button, TextField } from '@mui/material';
 import { useState } from 'react'
 import React from 'react';
+import Link from 'next/link'
+
 
 const firstState = {
      "first" : "",
@@ -20,16 +22,20 @@ function Signup() {
           //send form to backend for account creation
           setSignUpForm(firstState)
      }
-  return <div id="signup-form">
+  return <form id="signup-form">
                <div >
-                    <h1 id="signup-header">Let's create your account.</h1>
+                    <h1 id="signup-header">Let{"'"}s create your account.</h1>
                </div>
               <TextField required className="text-field" id="outlined-basic" name="first" value={signUpForm.first} onChange={(e)=> inputChange(e)} label="First Name" variant="outlined" />
-              <TextField required className="text-field" id="outlined-basic" name="last"value={signUpForm.last} onChange={(e)=>inputChange(e)} label="Last Name" variant="outlined" />
-              <TextField required className="text-field" id="outlined-basic" type="password" name="Password"value={signUpForm.Password} onChange={(e) => inputChange(e)} label="Password" variant="outlined" />
+              <TextField required className="text-field" id="outlined-basic2" name="last"value={signUpForm.last} onChange={(e)=>inputChange(e)} label="Last Name" variant="outlined" />
+              <TextField required className="text-field" id="outlined-basic3" type="password" name="Password"value={signUpForm.Password} onChange={(e) => inputChange(e)} label="Password" variant="outlined" />
               <Button id="signup-button" color="success" onClick={submit}variant="contained">Sign up</Button>
-              <p>Already have an account? <a href="/signin">Sign in. </a></p>
-          </div>;
+              <p>Already have an account? 
+                   <Link href="/signin">
+                      <a>Sign in. </a>
+                   </Link>
+               </p>
+          </form>;
 }
 
 export default Signup;
