258e9b71ee0ce04dec586a675314985b0af0a244
Adam Kozlowski <Akozlowski@catalpa-systems.com>
2022-02-11 17:32:40 -0600

    adds styling to cart

--- components/items/ItemInCart.js
+++ components/items/ItemInCart.js
@@ -1,20 +1,21 @@
 import React, {useContext} from 'react'
 import { CartContext } from '../cart/CartContext';
+import styles from '../../styles/Selection.module.css'
 
 function ItemInCart({item}) {
 
     const {addCartItem, subtractCartItem, eliminateCartItem } = useContext(CartContext);
 
     return (
-        <div className='itemInCart'>
-            <p>{item.name} x {item.quantity}</p>
-            <button onClick={() => eliminateCartItem(item)}>
+        <div className={styles.itemInCart}>
+            <p>{item.name} x {item.quantity} </p>
+            <button className={styles.cartButtonCancel} onClick={() => eliminateCartItem(item)}>
                 X
             </button>  
-            <button onClick={() => subtractCartItem(item)}> 
+            <button className={styles.cartButton} onClick={() => subtractCartItem(item)}> 
                 -
             </button>
-            <button onClick={() => addCartItem(item)}>
+            <button className={styles.cartButton} onClick={() => addCartItem(item)}>
                 +
             </button>
         </div>
